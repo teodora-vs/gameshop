0b8051825a074bb7752a8371c30b8780628c9ad7
TeodoraV1415 <teodorav1415@students-softuni.bg>
2023-11-05 21:29:11 +0200

    removed some comments

--- src/main/java/com/softuni/gameshop/config/SecurityConfiguration.java
+++ src/main/java/com/softuni/gameshop/config/SecurityConfiguration.java
@@ -18,10 +18,8 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.authorizeHttpRequests(
-                // Define which urls are visible by which users
                 authorizeRequests -> authorizeRequests
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                        // Allow anyone to see the home page, the registration page and the login form
                         .requestMatchers("/", "/login", "/register", "/login-error").permitAll()
                         .requestMatchers("/error").permitAll()
                         .requestMatchers("/admin").hasRole(UserRoleEnum.ADMIN.name())
@@ -30,10 +28,7 @@ public class SecurityConfiguration {
                 .formLogin(
                         formLogin -> {
                             formLogin
-                                    // redirect here when we access something which is not allwwwwwowed.
-                                    // also this is the page where we perform login.
                                     .loginPage("/login")
-                                    // The names of the input fields (in our case in auth-login.html)
                                     .usernameParameter("username")
                                     .passwordParameter("password")
                                     .defaultSuccessUrl("/home",true)
@@ -42,11 +37,8 @@ public class SecurityConfiguration {
                 ).logout(
                         logout -> {
                             logout
-                                    // the URL where we should POST something in order to perform the logout
                                     .logoutUrl("/logout")
-                                    // where to go when logged out?
                                     .logoutSuccessUrl("/")
-                                    // invalidate the HTTP session
                                     .invalidateHttpSession(true);
                         }
                 )
@@ -56,8 +48,6 @@ public class SecurityConfiguration {
 
     @Bean
     public UserDetailsService userDetailsService(UserRepository userRepository) {
-        // This service translates the mobilele users and roles
-        // to representation which spring security understands.
         return new UserDetailsServiceImpl(userRepository);
     }
 
